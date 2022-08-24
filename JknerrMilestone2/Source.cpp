5966b39cdff7fe351620e731c53aefbaae21d1cf
Jeffery Knerr <JKnerr1@my.gcu.edu>
2022-08-24 17:01:24 -0400

    Added structures

--- JknerrMilestone2/Source.cpp
+++ JknerrMilestone2/Source.cpp
@@ -3,6 +3,7 @@
 #include "Board.h"
 #include "playerturn.h"
 #include "gameover.h"
+#include "instruction.h"
 using namespace std;
 
 
@@ -14,15 +15,16 @@ int main()
     cout << "T I C K -- T A C -- T O E -- G A M E\n";
     cout << "FOR 2 PLAYERS\n";
     while (gameover()) {
+        printDetails();
         display_board();
         player_turn();
         gameover();
     }
     if (turn == 'X' && draw == false) {
-        cout << "Congratulations!Player with 'X' has won the game";
+        cout << "Congratulations!Player with 'O' has won the game";
     }
     else if (turn == 'O' && draw == false) {
-        cout << "Congratulations!Player with 'O' has won the game";
+        cout << "Congratulations!Player with 'X' has won the game";
     }
     else
         cout << "GAME DRAW!!!";
---
+++ JknerrMilestone2/instruction.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Board.h"
+#include <iostream>
+using namespace std;
+
+// Data Members
+int X;
+int O;
+
+
+// Member Functions
+void printDetails()
+{
+    cout << "Player 1 is X " "\n";
+    cout << "Player 2 is O " "\n";
+}
